e30b6393a3b3c72de077ca832fdb6de58dba3830
Joshua Brook <joshuabrookuk@gmail.com>
2020-07-21 16:22:16 +0100

    Created and passed dummy 'hello world' test.

--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,10 @@ test('renders learn react link', () => {
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+
+test('render hello world', () => {
+  const { getByText } = render(<App />);
+  const text = getByText(/hello world/i);
+  expect(text).toBeInTheDocument();
+});
