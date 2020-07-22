38ac06be33bf03ee67493f9790572d2fae769012
Olatunji Oduola <ola.oduola@gmail.com>
2020-07-22 12:08:16 +0100

    building form component with class

--- src/Form.js
+++ src/Form.js
@@ -1,5 +1,19 @@
-function Form() {
+import React from 'react';
 
+class Form extends React.Component {
+
+  onSubmitFn = (event) => {
+    console.log(event)
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.onSubmitFn}>
+        <input type='submit'/>
+      </form>
+    )
+
+  }
 }
 
 export default Form
