9d327cdd96235848c1d791efbeafcdf5c7ef5fe5
victorvallet <victorvallet8@gmail.com>
2020-07-22 11:27:02 +0200

    Removed unecessary files and code

--- src/App.test.js
+++ src/App.test.js
@@ -2,15 +2,11 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App component', () => {
+  test('render hello world', () => {
+    const { getByText } = render(<App />);
+    const text = getByText(/hello world/i);
+    expect(text).toBeInTheDocument();
+  });
 });
 
-
-test('render hello world', () => {
-  const { getByText } = render(<App />);
-  const text = getByText(/hello world/i);
-  expect(text).toBeInTheDocument();
-});
