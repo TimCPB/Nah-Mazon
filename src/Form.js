95e9b6df58c6c1db23a47a9bb8819c003baab7fb
victorvallet <victorvallet8@gmail.com>
2020-07-23 18:03:34 +0200

    Iterated over the list property to display every items on the page

--- src/Form.js
+++ src/Form.js
@@ -2,41 +2,47 @@ import React from 'react';
 // import ReactDOM from 'react-dom';
 
 class Form extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = { 
-      data: '',
-      visible: false
-    };
-    this.myChangeHandler = this.myChangeHandler.bind(this);
-  }
+    constructor(props) {
+        super(props)
+        this.state = {
+            data: '',
+            list: []
+        };
+        this.myChangeHandler = this.myChangeHandler.bind(this);
+        this.mySubmittedForm = this.mySubmittedForm.bind(this);
+    }
 
-  myChangeHandler = (event) => {
-    (event).preventDefault();
-    console.log(this.state)
-    this.setState({data: event.target.value});
-    console.log(this.state)
-  }
+    myChangeHandler = (event) => {
+        (event).preventDefault();
+        this.setState({ data: event.target.value });
+    }
 
-  mySubmittedForm = (event) => {
-    (event).preventDefault();
-    this.setState({visible: true});
-  }
+    mySubmittedForm = (event) => {
+        (event).preventDefault();
+        let array = []
+        array.push(this.state.data)
+        this.setState({ list: this.state.list.concat(array) });
+    }
 
-  render() {
-    return (
-      <div>    
-        <form onSubmit={this.mySubmittedForm}>
-          <h1>Product Info</h1>
-          <input type="text" value={this.state.data} onChange={this.myChangeHandler} />
-        <input type="submit" value="Submit" />
-        </form>
-     <p style = {{display: this.state.visible ? "block" : "none"}}>Product list: {this.state.data} </p>
-      </div>
-  
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.mySubmittedForm}>
+                    <h1>Product Info</h1>
+                    <input type="text" value={this.state.data} onChange={this.myChangeHandler} />
+                    <input type="submit" value="Submit" />
+                </form>
+                <p>Product list:</p>
+                <ul>
+                    {this.state.list.map((item, index) => (
+                        <li key={index}>{item}</li>
+                    ))}
+                </ul>
+            </div>
+            // style={{ display: this.state.visible ? "block" : "none" }}
 
-    );
-  }
+        );
+    }
 }
 
 export default Form
