6ad0e8ff6011c64e88074c68c72d946e49dc2811
Tim <TimCPB@github.com>
2020-07-23 16:24:01 +0100

    Add myChangeHandler and MySubmittedForm functions to Form.js - Submitted products are now displayed on the index page

--- src/Form.js
+++ src/Form.js
@@ -4,25 +4,34 @@ import React from 'react';
 class Form extends React.Component {
   constructor(props) {
     super(props)
-    this.state = { data: '' };
-    this.mySubmittedForm = this.mySubmittedForm.bind(this);
+    this.state = { 
+      data: '',
+      visible: false
+    };
+    this.myChangeHandler = this.myChangeHandler.bind(this);
   }
 
-  mySubmittedForm = (event) => {
+  myChangeHandler = (event) => {
     (event).preventDefault();
-    // this.props.onSubmit();
     console.log(this.state)
     this.setState({data: event.target.value});
     console.log(this.state)
   }
+
+  mySubmittedForm = (event) => {
+    (event).preventDefault();
+    this.setState({visible: true});
+  }
+
   render() {
     return (
       <div>    
         <form onSubmit={this.mySubmittedForm}>
           <h1>Product Info</h1>
-          <input type="text"/>
+          <input type="text" value={this.state.data} onChange={this.myChangeHandler} />
+        <input type="submit" value="Submit" />
         </form>
-     <p>Product list: {this.state.data} </p>
+     <p style = {{display: this.state.visible ? "block" : "none"}}>Product list: {this.state.data} </p>
       </div>
   
 
