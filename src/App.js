7d27bc1cbc67f4447108d47bc070aa10ae71ca41
Tim <TimCPB@github.com>
2020-07-24 12:00:56 +0100

    create Profile component in src/Profile.js and add router to App.js to create ./profile route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import Form from './Form';
+import Profile from './Profile';
+import {
+  // BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 
 function App() {
   return (
     <div className="App container">
-      <h1>Welcome to Not Amazon</h1>
-      <Form />
+      <Switch>
+        <Route path="/profile">
+          <Profile />
+        </Route>
+        <Route path="/">
+          <h1>Welcome to Not Amazon</h1>
+          <Form />
+        </Route>
+      </Switch>
     </div>
   );
 }
