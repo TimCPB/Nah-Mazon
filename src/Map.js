874ab7094354a8cedf8b87af153527518c13ca8a
Joshua Brook <joshuabrookuk@gmail.com>
2020-07-27 10:25:54 +0100

    Working on user set marker.

--- src/Map.js
+++ src/Map.js
@@ -5,20 +5,24 @@ export default function Map() {
   const [viewport, setViewport] = useState({
     width: 500,
     height: 500,
-    latitude: 51.505970,
-    longitude: -0.120455,
+    latitude: 51.5173633,
+    longitude: -0.0755167,
     zoom: 12,
   })
 
   var markerCords=[
-    51.505970,
-    -0.120455
+    // console.log(Form.state)
+    // Form.state.coordinates[0],
+    // Form.state.coordinates[1]
+    51.5173633,
+    -0.0655167
   ];
 
 
   return (
     <div>
-    <h3>It's a map</h3>
+    <h2>It's a map</h2><
+    h3>What's your location?</h3>
       <ReactMapGL
         {...viewport}
         mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
