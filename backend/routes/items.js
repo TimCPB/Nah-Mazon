8e4ebfb91dd0346da74295cc88d4f201e1c467e8
Olatunji Oduola <ola.oduola@gmail.com>
2020-07-27 15:59:59 +0100

    Created items routing info

--- backend/routes/items.js
+++ backend/routes/items.js
@@ -1,27 +1,21 @@
 const router = require('express').Router();
-let Exercise = require('../models/exercise.model');
+let Item = require('../models/item.model');
 
 router.route('/').get((req, res) => {
-  Exercise.find()
-    .then(exercises => res.json(exercises))
+  item.find()
+    .then(items => res.json(items))
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
 router.route('/add').post((req, res) => {
-  const username = req.body.username;
+  const title = req.body.title;
   const description = req.body.description;
-  const duration = Number(req.body.duration);
-  const date = Date.parse(req.body.date);
+  const price = Number(req.body.price);
 
-  const newExercise = new Exercise({
-    username,
-    description,
-    duration,
-    date,
-  });
+  const newItem = new Item({ title, description, price });
 
-  newExercise.save()
-  .then(() => res.json('Exercise added!'))
+  newItem.save()
+  .then(() => res.json("Your item has been added to Nah'Mazon!"))
   .catch(err => res.status(400).json('Error: ' + err));
 });
 
