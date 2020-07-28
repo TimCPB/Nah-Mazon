036d8a4d6ed7797a4f1e65ed5e4621f4a4324f43
victorvallet <victorvallet8@gmail.com>
2020-07-28 11:21:26 +0200

    Added onChange handlers to our input, and onSubmit  functiojn to output the state

--- src/FormProfile.js
+++ src/FormProfile.js
@@ -2,27 +2,60 @@ import React from 'react';
 
 class FormProfile extends React.Component {
   constructor(props) {
-      super(props)
-      this.state = {
-        name: '',
-        description: '',
-        address: ''
-      }
+    super(props)
+    this.state = {
+      name: '',
+      description: '',
+      address: ''
     }
+  }
 
-    render(){
-      return (
-        <div>
-          <form >
-            <input type="text" />
-            <input type="text" />
-            <input type="text" />
-            <input type="submit" value = "update" />
-          </form>
-        </div>
+  updateDetails = (e) => {
+    e.preventDefault();
+    console.log(this.state)
 
-      )
-    }
+  }
+
+  setBusinessName = (e) => {
+    e.preventDefault();
+    this.setState({ name: e.target.value })
+  }
+
+  setBusinessDesc = (e) => {
+    e.preventDefault();
+    this.setState({ description: e.target.value })
+  }
+
+  setBusinessAddress = (e) => {
+    e.preventDefault();
+    this.setState({ address: e.target.value })
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Enter your business details bellow</h2>
+        <form onSubmit={this.updateDetails}>
+          <div className="input-field col s6">
+            <input type="text" id="business_name" className="validate" value={this.state.name} onChange={this.setBusinessName} />
+            <label htmlFor="business_name">Business Name</label>
+          </div>
+
+          <div className="input-field col s6">
+            <input type="text" id="business_desc" className="validate" value={this.state.description} onChange={this.setBusinessDesc} />
+            <label htmlFor="business_desc">Business description</label>
+          </div>
+
+          <div className="input-field col s6">
+            <input type="text" id="business_address" className="validate" value={this.state.address} onChange={this.setBusinessAddress} />
+            <label htmlFor="business_addres">Business Address</label>
+          </div>
+          <input type="submit" value="update" />
+        </form>
+      </div>
+
+    )
+  }
 
 
 
