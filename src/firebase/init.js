b11a06495a92573115643d9685c352559da880e3
victorvallet <victorvallet8@gmail.com>
2020-07-29 19:26:28 +0200

    Initialized firebase project in the repo and  successfully built and deploy to hosting

---
+++ src/firebase/init.js
@@ -0,0 +1,14 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyChooD8TJNlh_49TpjSMQL4BpXwUPBACt8",
+    authDomain: "nah-mazon.firebaseapp.com",
+    databaseURL: "https://nah-mazon.firebaseio.com",
+    projectId: "nah-mazon",
+    storageBucket: "nah-mazon.appspot.com",
+    messagingSenderId: "1045129508735",
+    appId: "1:1045129508735:web:c478dfd1123d0c27f48c4a",
+    measurementId: "G-VZYQT9K3X0"
+};
+
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
