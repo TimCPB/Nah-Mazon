6af00e476275701b6626a201b85a3bb386a6fee4
victorvallet <victorvallet8@gmail.com>
2020-07-30 15:24:53 +0200

    Removed unecessary dom elements and style

--- src/components/BusinessProfileItems.js
+++ src/components/BusinessProfileItems.js
@@ -28,43 +28,12 @@ class ListProducts extends React.Component {
             })
     }
 
-    mySearchHandler = (e) => {
-        e.preventDefault();
-        this.setState({ search: e.target.value }
-        )
-    }
-
-    sortByPrice = (e) => {
-        e.preventDefault();
-        this.toggle()
-        var sortedList = []
-        if (this.state.toggle) {
-            sortedList = this.state.list.sort((product1, product2) => product1.price - product2.price)
-        }
-        else {
-            sortedList = this.state.list.sort((product1, product2) => product2.price - product1.price)
-        }
-        this.setState({ list: sortedList })
-    }
-
-    toggle = () => {
-        this.setState({ toggle: !this.state.toggle })
-    }
-
     render() {
         return (
             <div>
-                <form>
-                    <div className="input-field">
-                        <input id="search" type="search" required value={this.state.search} onChange={this.mySearchHandler} />
-                        <label className="prefix" htmlFor="search"><i className="material-icons">search</i></label>
-                        <i className="material-icons">close</i>
-                    </div>
-                </form>
-                <p>Product list:</p>
-                <button className="btn" type="button" onClick={this.sortByPrice}> sort by price </button>
                 <div className="row">
-                    {this.state.list.filter(item => item.title.toLocaleLowerCase().includes(this.state.search.toLocaleLowerCase())).map((item, index) => (
+                    <h4>My listed products</h4>
+                    {this.state.list.map((item, index) => (
                         <div key={index} className="card col s12 m3 hoverable center-align" style={{ minHeight: "80px", marginRight: "10px", cursor: "pointer" }}>
                             <div className="card-content">
                                 <span className="card-title"> {item.title}</span>
