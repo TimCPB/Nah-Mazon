2aed191c0c8025d6857e7000d26e5ad477a6a9b4
Olatunji Oduola <ola.oduola@gmail.com>
2020-07-31 12:55:56 +0100

    Updated homepage button

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,24 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 class Home extends React.Component {
-
-  render() {
-    return(
-      <div>
-      <h2><Link to="/create-profile">Business? Click here!</Link></h2>
-      <p><Link to="/list-items">Customer? Click here!</Link></p>
-      </div>
-    )
+	render() {
+		return (
+			<div>
+				<button className='btn'>
+					<Link className='white-text' to='/create-profile'>
+						Business Centre
+					</Link>
+					<br></br>
+				</button>
+				<button className='btn'>
+					<Link className='white-text' to='/list-items'>
+						View Items
+					</Link>
+				</button>
+			</div>
+		);
+	}
 }
-}
 
-export default Home
+export default Home;
