6506087c83da5bbac300178117a07f1037ebf7dd
victorvallet <victorvallet8@gmail.com>
2020-07-31 12:36:17 +0200

    Initialized firebase and firebase database

---
+++ src/firebase/init.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyChooD8TJNlh_49TpjSMQL4BpXwUPBACt8",
+    authDomain: "nah-mazon.firebaseapp.com",
+    databaseURL: "https://nah-mazon.firebaseio.com",
+    projectId: "nah-mazon",
+    storageBucket: "nah-mazon.appspot.com",
+    messagingSenderId: "1045129508735",
+    appId: "1:1045129508735:web:c478dfd1123d0c27f48c4a",
+    measurementId: "G-VZYQT9K3X0"
+};
+
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+const db = firebaseApp.firestore();
+
+export default db;
