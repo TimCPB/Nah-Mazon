3e7f91ada5ca084ca20372ac4078df907de1bc69
victorvallet <victorvallet8@gmail.com>
2020-07-31 14:34:44 +0200

    Added latest version of the Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,29 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 class Home extends React.Component {
-
   render() {
-    return(
-      <div>
-      <h2><Link to="/create-profile">Business? Click here!</Link></h2>
-      <p><Link to="/list-items">Customer? Click here!</Link></p>
+    return (
+      <div className='container centre-align'>
+        <div className='row' style={{ marginLeft: "25%" }}>
+          <div className='col'>
+            <button className='btn'>
+              <Link className='white-text' to='/create-profile'>
+                Business Hub
+							</Link>
+            </button>
+          </div>
+          <div className='col'>
+            <button className='btn'>
+              <Link className='white-text' to='/list-items'>
+                Customer Hub
+							</Link>
+            </button>
+          </div>
+        </div>
       </div>
-    )
-}
+    );
+  }
 }
 
-export default Home
+export default Home;
